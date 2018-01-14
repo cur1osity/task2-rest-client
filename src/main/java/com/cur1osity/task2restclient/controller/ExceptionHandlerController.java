60bd2b23ad392a4e8e02f3906a01ea4305a3c36e
cur1osity <marcin.gajc@gmail.com>
2018-01-14 20:16:28 +0100

    Exception handling trello part

--- src/main/java/com/cur1osity/task2restclient/controller/ExceptionHandlerController.java
+++ src/main/java/com/cur1osity/task2restclient/controller/ExceptionHandlerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 import java.io.IOException;
 
@@ -19,16 +20,30 @@ public class ExceptionHandlerController {
     @Autowired
     private TaskClientController taskController;
 
+    @Autowired
+    private TrelloController trelloController;
+
     @ExceptionHandler(HttpStatusCodeException.class)
-    public String handleClientError(HttpStatusCodeException ex, Model model) throws ServiceUnavailableEx, IOException {
+    public String handleClientError(HttpStatusCodeException ex, Model model) throws IOException {
+
         if(ex.getStatusCode() == HttpStatus.BAD_REQUEST) {
             MessageDto dto = mapper.readValue(ex.getResponseBodyAsByteArray(), MessageDto.class);
             model.addAttribute("bad_request_error", dto.getMessages());
+            return taskController.findAll(model);
         }
-        if(ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+
+       if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
             MessageDto dto = mapper.readValue(ex.getResponseBodyAsByteArray(), MessageDto.class);
             model.addAttribute("not_found_error", dto.getMessages());
+            return taskController.findAll(model);
         }
-        return taskController.findAll(model);
+
+        if (ex.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+            MessageDto dto = mapper.readValue(ex.getResponseBodyAsByteArray(), MessageDto.class);
+            model.addAttribute("bad_request_error", dto.getMessages());
+            return trelloController.findAll(model);
+        }
+
+        return "error";
     }
 }
