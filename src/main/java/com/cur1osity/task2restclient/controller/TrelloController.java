03cca5fa08e8634a73be2c83b3151a476a44e0a2
cur1osity <marcin.gajc@gmail.com>
2018-01-15 12:10:15 +0100

    Dodane createdTrelloCardDto

--- src/main/java/com/cur1osity/task2restclient/controller/TrelloController.java
+++ src/main/java/com/cur1osity/task2restclient/controller/TrelloController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/trello")
@@ -46,11 +45,17 @@ public class TrelloController {
         return "trello";
     }
 
-
     @PostMapping
-    public String create(@ModelAttribute("newTrelloCard") TrelloCardDto trelloCardDto, RedirectAttributes model) {
-        model.addFlashAttribute("message_post", "success");
-        trelloClientService.createTrelloCard(trelloCardDto);
-        return "redirect:/trello";
+    public String create(@ModelAttribute("newTrelloCard") TrelloCardDto trelloCardDto, Model model) {
+
+        try {
+            model.addAttribute("tasks", service.findAll());
+            model.addAttribute("newTask", new TaskDto());
+            model.addAttribute("trelloBoards", trelloClientService.findAll());
+            model.addAttribute("trelloCard", trelloClientService.createTrelloCard(trelloCardDto));
+        } catch (ServiceUnavailableEx ex) {
+//            noService(model);
+        }
+        return "trello";
     }
 }
--- src/main/java/com/cur1osity/task2restclient/service/TrelloClientService.java
+++ src/main/java/com/cur1osity/task2restclient/service/TrelloClientService.java
@@ -1,5 +1,6 @@
 package com.cur1osity.task2restclient.service;
 
+import com.cur1osity.task2restclient.domain.CreatedTrelloCardDto;
 import com.cur1osity.task2restclient.domain.TrelloBoardDto;
 import com.cur1osity.task2restclient.domain.TrelloCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,6 @@ public class TrelloClientService {
 
     public List<TrelloBoardDto> findAll()  {
 
-//            restTemplate.getForObject(resource, TrelloBoardDto[].class);
-
         return Arrays.stream(restTemplate.getForObject(resource, TrelloBoardDto[].class)).collect(Collectors.toList());
     }
 
@@ -36,8 +35,7 @@ public class TrelloClientService {
         return restTemplate.getForObject(idResource, TrelloBoardDto.class, id);
     }
 
-    public TrelloCardDto createTrelloCard(TrelloCardDto trelloCardDto) {
-        return restTemplate.postForObject(resource, trelloCardDto, TrelloCardDto.class);
+    public CreatedTrelloCardDto createTrelloCard(TrelloCardDto trelloCardDto) {
+        return restTemplate.postForObject(resource, trelloCardDto, CreatedTrelloCardDto.class);
     }
-
 }
