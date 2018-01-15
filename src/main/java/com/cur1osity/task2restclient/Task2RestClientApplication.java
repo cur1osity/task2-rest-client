bbf42ac8c9432e1f8560488c487b6ad5115565b5
cur1osity <marcin.gajc@gmail.com>
2018-01-15 17:13:04 +0100

    Eureka + docker init

--- src/main/java/com/cur1osity/task2restclient/Task2RestClientApplication.java
+++ src/main/java/com/cur1osity/task2restclient/Task2RestClientApplication.java
@@ -2,9 +2,9 @@ package com.cur1osity.task2restclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class Task2RestClientApplication {
 
